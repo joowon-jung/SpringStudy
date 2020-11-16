967d6cd33cb85189797a870813a825dda917e754
Joowon Jung <jjoowon.jung@gmail.com>
2020-11-16 21:06:48 +0900

    Spring Framework에서 지원하는 AOP 공부

---
+++ 04.AOP/src/main/java/spring/service/aop/advice/BeforeLogAdvice.java
@@ -0,0 +1,24 @@
+package spring.service.aop.advice;
+
+import java.lang.reflect.Method;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+/*
+ * 	Advice :: 선 처리 
+ */
+public class BeforeLogAdvice implements MethodBeforeAdvice {
+
+	@Override
+	public void before(Method method, Object[] args, Object target) throws Throwable {
+		
+		System.out.println("\n=====================================");
+		System.out.println("[before LOG] " + getClass()+".before() start.....");
+		System.out.println("[before LOG] targetObject call Method : " + method);
+		if (args.length != 0) {
+			System.out.println("[before LOG] targetObject method 전달 argument : " + args[0]);
+		}
+		System.out.println("[before LOG] " + getClass()+".before() end.....");
+	}
+
+}
